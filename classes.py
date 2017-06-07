f00d428e39e9dc3c34e85e23f9bc7d6af9601a28
Sterling Vangeloff <werling44@gmail.com>
2017-06-06 21:16:37 -0600

    Added some functionality to ui.  Parse now stores found emails into objects which displayed/scrolled through in the UI

---
+++ classes.py
@@ -0,0 +1,6 @@
+class parsedEmail():
+    def __init__(self, subject, sender, time, body):
+        self.subject = subject
+        self.sender = sender
+        self.time = time
+        self.body = body
--- smtp.py
+++ smtp.py
@@ -10,8 +10,7 @@ from tkinter import messagebox
 import tkinter.ttk as ttk
 from tkinter.scrolledtext import ScrolledText
 from configparser import ConfigParser
-
-
+from classes import *
 
 # from pygeocoder import Geocoder
 # import pandas as pd
@@ -31,6 +30,8 @@ EMAIL_FOLDER = "INBOX"
 
 def process_mailbox(M):
     i=0
+    global emailArray
+    emailArray = []
     """
     Do something with emails messages in the folder.
     For the sake of this example, print some headers.
@@ -93,14 +94,20 @@ def process_mailbox(M):
 
 
         if "free food" in str(body).lower():
-            print('Message %s: %s' % (num, subject))
-            print("Local Date:", local_date.strftime("%a, %d %b %Y %H:%M:%S"))
-            print("Sender = " + sender[0].split("<")[1][:-1])
-            print(body)
+            time = local_date.strftime("%a, %d %b %Y %H:%M:%S")
+            sender =  sender[0].split("<")[1][:-1]
+            # print('Message %s: %s' % (num, subject))
+            # print("Local Date:", local_date.strftime("%a, %d %b %Y %H:%M:%S"))
+            # print("Sender = " + sender)
+            # print(body)
+            emailObject = parsedEmail(subject, sender, time, body)
+            emailArray.append(emailObject)
 
-        # i = i+1
-        # if i == 10:
-        #     break
+
+        i = i+1
+        if i == 200:
+            print("Parsing Done")
+            break
 
 
 M = imaplib.IMAP4_SSL('imap.gmail.com')
@@ -129,25 +136,56 @@ def start_process():
         M.close()
     else:
         print("ERROR: Unable to open mailbox ", rv)
+    initalEmailDisplay()
     M.logout()
+
+def initalEmailDisplay():
+    length = str(len(emailArray))
+    emailLabel.config(text='1' + '/' + length)
+    fillEmailFields()
+
+def fillEmailFields():
+    emailPosition = int(emailLabel.cget("text").split('/')[0]) - 1
+    currentEmail = emailArray[emailPosition]
+    subjectEntry.delete(0, END)
+    fromEntry.delete(0, END)
+    timeEntry.delete(0, END)
+    bodyScrolledText.delete(1.0, END)
+    subjectEntry.insert(0, currentEmail.subject)
+    fromEntry.insert(0, currentEmail.sender)
+    timeEntry.insert(0, currentEmail.time)
+    bodyScrolledText.insert(1.0, currentEmail.body)
+
+def nextEmail(number):
+    emailLabelText = emailLabel.cget("text").split('/')
+    currentEmailPosition = int(emailLabelText[0]) - 1
+    totalEmails = int(emailLabelText[1])
+    if totalEmails == 0:
+        return
+    else:
+        nextEmailPosition = (currentEmailPosition + number) % totalEmails
+        emailLabel.config(text=str(nextEmailPosition + 1) + '/' + str(totalEmails))
+        fillEmailFields()
 
 def doNothing():
     return
 
 root = Tk()
 root.title("Free Food")
+# Menu Bar Creation
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 root.config(menu=menubar)
-
+# Creating Left and Right Boxes of ui
 leftBox = Frame(root, borderwidth=0)
 rightBox = Frame(root, borderwidth=5)
 
 leftBox.pack(side=LEFT)
 rightBox.pack(side=RIGHT)
-#Creation of Boxes
+
+#Creation of ui elements
 # Left
 subjectLabel = Label(leftBox, text='Subject')
 subjectEntry = Entry(leftBox, width=100)
@@ -172,14 +210,21 @@ groupEntry = Entry(rightBox, width=50)
 locationLabel = Label(rightBox, text='Location')
 locationEntry = Entry(rightBox, width=50)
 
+timeLabel = Label(rightBox, text='Time')
+timeEntry = Entry(rightBox, width=50)
+
+dateLabel = Label(rightBox, text='Date')
+dateEntry = Entry(rightBox, width=50)
+
 descriptionLabel = Label(rightBox, text='Subject')
 descriptionScrolledText = ScrolledText(rightBox, height=20, width=50, undo=True)
 
-previousButton = Button(rightBox, text="<Prev", width=25, command=lambda: doNothing())
+emailLabel = Label(rightBox, text='0/0')
+previousButton = Button(rightBox, text="<Prev", width=25, command=lambda: nextEmail(-1))
 confirmButton = Button(rightBox, text="Confirm", width=25,background="red",  command=lambda: doNothing())
-nextButton = Button(rightBox, text="Next>", width=25,  command=lambda: doNothing())
+nextButton = Button(rightBox, text="Next>", width=25,  command=lambda: nextEmail(1))
 
-#Adding boxes to grid
+#Adding ui elements
 # Left
 subjectLabel.grid(row=9)
 subjectEntry.grid(row=9, column=1, padx=10, pady=2)
@@ -192,25 +237,30 @@ timeEntry.grid(row=11, column=1, padx=10, pady=2)
 
 bodyLabel.grid(row=12)
 bodyScrolledText.grid(row=12, column=1, padx=10, pady=10)
-
 # Right
-
-parseButton.grid(column=1, row = 7, pady = 2)
+parseButton.grid(column=1, row = 7, pady = 2, columnspan=3)
 
 titleLabel.grid(row=9)
-titleEntry.grid(row=9, column=1, padx=10, pady=2)
+titleEntry.grid(row=9, column=1, padx=10, pady=2,columnspan=3)
 
 groupLabel.grid(row=10)
-groupEntry.grid(row=10, column=1, padx=10, pady=2)
+groupEntry.grid(row=10, column=1, padx=10, pady=2,columnspan=3)
 
 locationLabel.grid(row=11)
-locationEntry.grid(row=11, column=1, padx=10, pady=2)
+locationEntry.grid(row=11, column=1, padx=10, pady=2,columnspan=3)
 
-descriptionLabel.grid(row=12)
-descriptionScrolledText.grid(row=12, column=1, padx=10, pady=10)
+timeLabel.grid(row=13)
+timeEntry.grid(row=13, column=1, padx=10, pady=2,columnspan=3)
 
+dateLabel.grid(row=12, column=0)
+dateEntry.grid(row=12, column=1, padx=10, pady=2,columnspan=3)
+
+descriptionLabel.grid(row=14)
+descriptionScrolledText.grid(row=14, column=1, padx=10, pady=10,columnspan=3)
+
+emailLabel.grid(row=20, column=2)
 previousButton.grid(column=1, row = 20, pady = 10, sticky=W)
-confirmButton.grid(column=1, row = 21, pady = 10)
-nextButton.grid(column=1, row = 20, pady = 10, sticky=E)
+confirmButton.grid(column=2, row = 21, pady = 10)
+nextButton.grid(column=3, row = 20, pady = 10, sticky=E)
 
 root.mainloop()
